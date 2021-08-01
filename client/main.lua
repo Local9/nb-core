130d18081aeb5136c5fc853242d75f3f8f8cfb08
negbook <negbook@gmail.com>
2021-08-02 01:56:15 +0800

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,7 @@
 CreateThread(function()
-	while not NetworkIsSessionStarted() do
-		Wait(33)
-	end
+    while not NetworkIsSessionStarted() do
+        Wait(33)
+    end
     TriggerServerEvent('NB:OnPlayerSessionStart')
     return 
 end)
