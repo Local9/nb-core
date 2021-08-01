dd452923d5d2791834b2058024e649f1ae1926c5
negbook <negbook@gmail.com>
2021-08-02 03:01:29 +0800

    add callback

--- shared_main.lua
+++ shared_main.lua
@@ -18,6 +18,31 @@ if IsServer() then
     CreateThread(function()
         print("[NBCore] Server Loaded")
     end)
+end 
+if IsClient() then 
+    CreateThread(function()
+        local SpawnPlayer = function()
+            exports.spawnmanager:spawnPlayer({
+                    x = -802.311, y = 175.056, z = 72.8446,heading = 0.0,model = `a_m_y_hipster_01`,
+                    skipFade = false
+                }, function()
+                ShutdownLoadingScreen()
+                ShutdownLoadingScreenNui()
+                FreezeEntityPosition(PlayerPedId(), false)
+            end)
+        end
+        while not NetworkIsSessionStarted() do
+            Wait(0)
+        end
+        SpawnPlayer()
+        TriggerServerEvent('NB:OnPlayerSessionStart')
+        TriggerEvent('NB:OnPlayerSessionStart')
+        --print("NB:OnPlayerSessionStart")
+        return 
+    end)
+end 
+
+if IsServer() then 
     local RegisterServerCallback
     RegisterServerCallback = function(name,fn)
         local resname = GetCurrentResourceName()
@@ -45,26 +70,6 @@ if IsServer() then
     NB.RegisterServerCallback = RegisterServerCallback --https://github.com/negbook/ServerCallback
 end 
 if IsClient() then 
-    CreateThread(function()
-        local SpawnPlayer = function()
-            exports.spawnmanager:spawnPlayer({
-                    x = -802.311, y = 175.056, z = 72.8446,heading = 0.0,model = `a_m_y_hipster_01`,
-                    skipFade = false
-                }, function()
-                ShutdownLoadingScreen()
-                ShutdownLoadingScreenNui()
-                FreezeEntityPosition(PlayerPedId(), false)
-            end)
-        end
-        while not NetworkIsSessionStarted() do
-            Wait(0)
-        end
-        SpawnPlayer()
-        TriggerServerEvent('NB:OnPlayerSessionStart')
-        TriggerEvent('NB:OnPlayerSessionStart')
-        --print("NB:OnPlayerSessionStart")
-        return 
-    end)
     local TriggerServerCallback
     TriggerServerCallback = function(name,fn,...)
         local resname = GetCurrentResourceName()
@@ -80,4 +85,3 @@ if IsClient() then
     end 
     NB.TriggerServerCallback = TriggerServerCallback --https://github.com/negbook/ServerCallback
 end 
-
