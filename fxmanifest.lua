75d1b46bf4eb90bcb744ec28d4235dcd3da56f81
negbook <negbook@gmail.com>
2021-08-03 01:31:01 +0800

    sa-mp?

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -14,14 +14,14 @@ client_scripts {
 'client/functions.lua',
 'client/events.lua',
 'client/session.lua',
-'client.lua'
+'main_client.lua'
 }
 server_scripts{
 	'server/functions.lua',
 	'server/callbacks.lua',
 	'server/events.lua',
 	'server/session.lua',
-	'server.lua'
+	'main_server.lua'
 }
 
 dependencies {
