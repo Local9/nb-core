b1e0ce0aea0253beabe7618377f625bf8f154115
negbook <negbook@gmail.com>
2021-08-09 23:04:23 +0800

    wtf????

--- server/functions.lua
+++ server/functions.lua
@@ -54,41 +54,6 @@ NB.MakeSureTempSomethingExist = function(...)
 	end 
 	return nil 
 end 
-NB.GetTempSomthing = function(...)
-	local GetTempSomthing_ = function(...)
-		local args = {...}
-		local thisparent = NB._temp_
-		local lastv = nil 
-		
-		for i=1,#args do 
-			local v = tostring(args[i])
-			if thisparent[v] == nil then 
-				thisparent[v] = {} 
-			end 
-			if i ~= #args then 
-				thisparent = thisparent[v]
-			else 
-				local t = {}
-				for i,v in pairs(args) do 
-					t[i] = v 
-				end 
-				if thisparent[v] == nil then 
-					thisparent[v] = nil
-				else 
-					return thisparent[v]
-				end 
-				return thisparent[v]
-			end 
-		end 
-		return nil 
-	end 
-	local args = {...}
-	local lastarg = args[#args]
-	table.remove(args,#args)
-	local args2 = args 
-	local wtf = GetTempSomthing_(table.unpack(args2))[lastarg]
-	return wtf
-end 
 
 NB.IsTempSomthingExist = function(...)
 	local args = {...}
@@ -116,6 +81,46 @@ NB.IsTempSomthingExist = function(...)
 			end 
 		end 
 		return false 
+end 
+
+NB.GetTempSomthing = function(...)
+	if NB.IsTempSomthingExist("CitizenDatas",CitizenID,tablename,dataname) then 
+		local GetTempSomthing_ = function(...)
+			local args = {...}
+			local thisparent = NB._temp_
+			local lastv = nil 
+			
+			for i=1,#args do 
+				local v = tostring(args[i])
+				if thisparent[v] == nil then 
+					thisparent[v] = {} 
+				end 
+				if i ~= #args then 
+					thisparent = thisparent[v]
+				else 
+					local t = {}
+					for i,v in pairs(args) do 
+						t[i] = v 
+					end 
+					if thisparent[v] == nil then 
+						thisparent[v] = nil
+					else 
+						return thisparent[v]
+					end 
+					return thisparent[v]
+				end 
+			end 
+			return nil 
+		end 
+		local args = {...}
+		local lastarg = args[#args]
+		table.remove(args,#args)
+		local args2 = args 
+		local wtf = GetTempSomthing_(table.unpack(args2))[lastarg]
+		return wtf
+	else 
+		return nil 
+	end 
 end 
 
 NB.SendClientMessage = function(playerId, color, message)
