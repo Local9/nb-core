de69ff6845d1bf3cdd72c38b4f5c2ff58fba44e8
negbook <negbook@gmail.com>
2021-08-18 17:13:10 +0800

    clean

--- com/menu/com.menu.PauseMenu/main.lua
+++ com/menu/com.menu.PauseMenu/main.lua
@@ -2,23 +2,4 @@ if IsClient() then
 --bridge
 
 
-
-local start = BeginScaleformMovieMethodOnFrontend
-local send = function (...)
-    local tb = {...}
-    for i=1,#tb do
-        if type(tb[i]) == "number" then 
-            if math.type(tb[i]) == "integer" then
-                    ScaleformMovieMethodAddParamInt(tb[i])
-            else
-                    ScaleformMovieMethodAddParamFloat(tb[i])
-            end
-        elseif type(tb[i]) == "string" then ScaleformMovieMethodAddParamTextureNameString(tb[i])
-        elseif type(tb[i]) == "boolean" then ScaleformMovieMethodAddParamBool(tb[i])
-        end
-    end 
-	EndScaleformMovieMethod()
-end
-
-
 end 
