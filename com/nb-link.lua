da745f9c16b5aebad48ac6963e617618f08f205a
negbook <negbook@gmail.com>
2021-08-21 04:36:17 +0800

    up

--- com/nb-link.lua
+++ com/nb-link.lua
@@ -13,14 +13,17 @@ if IsShared() then
 	NB.Cache.SetPropSlotValue  = function(...) return com.lua.utils.Table.SetTableSomething(NB["_CACHE_"],...) end
 	NB.Cache.IsPropValueExist = function(...) return com.lua.utils.Table.IsTableSomthingExist(NB["_CACHE_"],...) end 
 	NB.Cache.GetPropSlotValue = function(...) return com.lua.utils.Table.GetTableSomthing(NB["_CACHE_"],...) end  
-	NB.InsertPropSlot = function(...) return com.lua.utils.Table.InsertTableSomethingTable(NB["_CACHE_"],...) end
-	NB.RemovePropSlot = function(...) return com.lua.utils.Table.RemoveTableSomethingTable(NB["_CACHE_"],...) end
+	NB.Cache.InsertPropSlot = function(...) return com.lua.utils.Table.InsertTableSomethingTable(NB["_CACHE_"],...) end
+	NB.Cache.RemovePropSlot = function(...) return com.lua.utils.Table.RemoveTableSomethingTable(NB["_CACHE_"],...) end
+	NB.Cache = setmetatable(NB.Cache,{__call = function(self,...) return NB.Cache.SetPropSlotValue(...) end })
+	NB.GetCache = function(...) if not NB.Cache.IsPropValueExist(...) then return nil end return NB.Cache.GetPropSlotValue(...) end 
 	NB.LoadDataSheet = com.game.Shared.Load.LoadDataSheet
 	NB.Random = com.lua.utils.Math.getRandomNumber
 	NB.RandomFloat = com.lua.utils.Math.getRandomFloat
 	NB.AsyncLimit = com.lua.utils.Async.CreateLimit --namespace,limited,fnwithWaitAndReturnInside
 	NB.AsyncSeries = com.lua.utils.Async.CreateSeries --namespace,fnwithWaitAndReturnInside
-	
+	NB.RandomSeed = com.lua.utils.Random.MakeSeed
+	NBRandomSeed = NB.RandomSeed
 end 
 if IsServer() then 
 	NB.GetLicense = com.game.Server.License.Get
--- import.lua
+++ import.lua
@@ -1,3 +1,7 @@
+--IMPORT--
 if GetCurrentResourceName() ~= "nb-core" then --a must, otherwise function becomes table in main framework script
 NB = exports['nb-core']:GetSharedObject()
 end 
+--Shared Globals--
+NBRandomSeed = NB["RandomSeed"]
+
