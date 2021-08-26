d61a4ab9a48e4278fdf07427ebd163238092265e
negbook <negbook@gmail.com>
2021-08-26 17:16:43 +0800

    stored sql

--- server/DB/citizens.lua
+++ server/DB/citizens.lua
@@ -1,4 +1,7 @@
 local CitizenSaveQueryid = {}
+local CitizenLastQuery = {}
+local LastQueryid = {}
+local LastQuery = {}
 DB.Citizen.GetData = function (citizenID)
 	if not NB.Cache.IsExist("CITIZEN",citizenID) then 
 		NB.Cache.Set("CITIZEN",citizenID,{})
@@ -48,20 +51,35 @@ DB.Citizen.CacheToSql = function(citizenID,tablename,dataslot)
 		end 
 	end 
 	if type(dataslot) == 'table' then 
+		local datastore = {}
 		local querys = {}
 		local datadefines = {
-			 
+			 citizenID
 		}
 		for dataslot_,data_ in pairs(dataslot) do 
-			datadefines[dataslot_] = covertDatas(data_)
-			print(dataslot_)
+			table.insert(querys,--[[dataslot_..' = ]]'?')
+			table.insert(datadefines,covertDatas(data_))
+			table.insert(datastore,{[dataslot_] = covertDatas(data_)})
 		end 
 		querys = table.concat(querys,",")
-		if not CitizenSaveQueryid[tablename] then 
-			CitizenSaveQueryid[tablename] = com.lua.utils.Remote.mysql_storeSync("UPDATE "..tablename.." SET ? WHERE ?")
+		local newquerystring = 'UPDATE '..tablename..' SET '..querys..' WHERE ?'
+		if not CitizenSaveQueryid[citizenID] then 
+			CitizenLastQuery[citizenID] = {}
+			CitizenSaveQueryid[citizenID] = {}
 		end 
-		
-		NB.Utils.Remote.mysql_execute(CitizenSaveQueryid[tablename], {datadefines, {['citizen_id'] = citizenID}})
+		if not CitizenSaveQueryid[citizenID][tablename] or CitizenLastQuery[citizenID][tablename]~= newquerystring then 
+			if LastQuery[tablename] == newquerystring then 
+				CitizenLastQuery[citizenID][tablename] = LastQuery[tablename]
+				CitizenSaveQueryid[citizenID][tablename] = LastQueryid[tablename]
+			else
+				LastQuery[tablename] = newquerystring
+				LastQueryid[tablename] = com.lua.utils.Remote.mysql_storeSync(newquerystring)
+				CitizenLastQuery[citizenID][tablename] = LastQuery[tablename]
+				CitizenSaveQueryid[citizenID][tablename] = LastQueryid[tablename]
+			end 
+		end 
+		table.insert(datastore,{['citizen_id'] = citizenID})
+		NB.Utils.Remote.mysql_execute(CitizenSaveQueryid[citizenID][tablename], datastore)
 	end 
 end 
 DB.Citizen.AllCachesToSql = function(citizenID,isClear)
@@ -98,8 +116,6 @@ DB.Citizen.AllCachesToSql = function(citizenID,isClear)
 					print('Citizen '..results[i].citizenID..' Data is saved.\n:',results[i].result)
 				end 
 			end 
-			
-			
 		end)
 	end 
 end 
@@ -116,16 +132,13 @@ DB.Citizen.Create = function(citizenID,license,cb)
 end 
 CreateThread(function()
 	Wait(10000)
-	
 	NB.Threads.CreateLoop("saveAllCacheDB",10000,function()
 		DB.Citizen.AllCachesToSql()
 	end)
 end )
-
 NB.DeleteCitizenDataCache = function(...)
 	return NB.Cache.Clear("CITIZEN",...)
 end 
-
 NB.GetCitizenDataCache = function(citizenID,tablename,dataslot)
 	return NB.Cache.Get("CITIZEN",citizenID,tablename,dataslot) or DB.Citizen.SqlToCache(citizenID,tablename,dataslot)
 end 
@@ -168,7 +181,6 @@ NB.SetCitizenPackedDataCache = function(citizenID,tablename,dataslot,datas,isCom
 	NB.Cache.Set("CITIZEN",citizenID,tablename,"packeddata",_data)
 	--NB.Cache.Set("CITIZEN",citizenID,tablename,"packeddata",dataslot,datas)
 end 
-
 NB.RegisterNetEvent('NB:Citizen:SavePosition', function(coords,heading)
 	if coords and heading then 
 		local playerData = NB.GetPlayerDataFromId(tonumber(source))
@@ -180,6 +192,7 @@ NB.RegisterNetEvent('NB:Citizen:SavePosition', function(coords,heading)
 			y = com.lua.utils.Math.toFixed(y,2)
 			z = com.lua.utils.Math.toFixed(z,2)
 			heading = com.lua.utils.Math.toFixed(heading,2)
+			NB.SetCitizenDataCache(citizenID,'citizens','test',"test")
 			NB.SetCitizenPackedDataCache(citizenID,'citizens','position',{x=x,y=y,z=z,heading=heading})
 			NB.TriggerEvent("NB:log","[Citizen:"..citizenID.."] position Saved")
 		end 
@@ -214,4 +227,3 @@ NB.RegisterServerCallback("NB:GetCharacterPackedData",function(playerId,cb,datat
 		end 
 	end 
 end )
-
